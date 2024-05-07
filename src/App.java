5d043233c5cd796f3e2e2e46f4def2ff087df9c4
FailRANK <franknguyen2006@gmail.com>
2024-05-07 11:23:34 -0400

    First Commit

--- src/App.java
+++ src/App.java
@@ -16,13 +16,14 @@ public class App extends JFrame {
     static JButton Button2 = new JButton();
     static JButton Button3 = new JButton();
     static JButton Button4 = new JButton();
+    static int Correct = 0;
+    static int QuestionLevel = 0;
     public static void main(String[] args) throws Exception {
         Dimension Size = Toolkit.getDefaultToolkit().getScreenSize();
 
         Frame.setLayout(null);
         Frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         Frame.setSize((int)Size.getWidth(), (int)Size.getHeight());
-        Frame.setVisible(true);
 
         Label.setLocation(600, 0);
         Label.setSize(1000, 500);
@@ -36,6 +37,28 @@ public class App extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Button.setVisible(false);
+                Frame.add(Button1);
+                Frame.add(Button2);
+                Frame.add(Button3);
+                Frame.add(Button4);
+                Question();
+            }
+        });
+
+        Button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Status.setText("Correct!");
+                Increse(1);
+                Question();
+            }
+        });
+
+        Button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Status.setText("Wrong!");
+                Increse(0);
                 Question();
             }
         });
@@ -44,44 +67,43 @@ public class App extends JFrame {
         Status.setSize(1000, 500);
         Status.setFont(new Font("Arial", Font.BOLD, 50));
 
+        Button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Status.setText("Wrong!");
+                Increse(0);
+            }
+        });
+        
         Frame.add(Label);
         Frame.add(Button);
         Frame.add(Status);
+        Frame.setVisible(true);
     }
 
     public static void Question() {
 
-        Label.setText("Question 1: Yes or No");
+        String[] Questions = {"Qustion 1: Yes or No", "Qustion 2: No or Yes", "Question 3: CHEESSE", "Question 4: FAFAF", "Qustion 5: SADSADA"};
+        String[] Button1Text = {"Yes", "No", "CHEESSE", "FAFAF", "SADSADA"};
+        String[] Button2Text = {"No", "Yes", "CHEESSE", "IDK", "what"};
 
-        Button1.setText("Yes");
+        System.out.print(QuestionLevel);
+
+        Label.setText(Questions[QuestionLevel]);
+
+        Button1.setText(Button1Text[QuestionLevel]);
         Button1.setLocation(600, 500);
         Button1.setSize(300, 200);
         Button1.setFont(new Font("Arial", Font.BOLD, 30));
-        Button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Status.setText("Correct!");
-                Button1.setVisible(false);
-                Button2.setVisible(false);
-                Label.setVisible(false);
-            }
-        });
 
-        Button2.setText("No");
+        Button2.setText(Button2Text[QuestionLevel]);
         Button2.setLocation(1200, 500);
         Button2.setSize(300, 200);
         Button2.setFont(new Font("Arial", Font.BOLD, 30));
-        Button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Status.setText("Wrong!");
-                Button1.setVisible(false);
-                Button2.setVisible(false);
-                Label.setVisible(false);
-            }
-        });
+    }
 
-        Frame.add(Button1);
-        Frame.add(Button2);
+    public static void Increse(int Value) {
+        QuestionLevel += 1;
+        Correct += Value;
     }
 }
